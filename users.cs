2bfb1a6a0db002cee9d1dd2db64a25825339ee5a
Maha <mahanubi829@gmail.com>
2023-09-17 14:20:39 +0300

    first commit

---
+++ users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.DayOne
+{
+    public class users
+    {
+        
+        
+            public void display()
+            {
+                System.Console.WriteLine("first day lab");
+            }
+
+        
+    }
+}
